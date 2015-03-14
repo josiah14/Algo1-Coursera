d1e1f2c838f7f78cf42df419e7e15fb0d1057a83
josiah14 <praenato14@gmail.com>
2015-03-13 23:06:54 -0500

    finish Karatsuba haskell test demonstrations

--- Ch1-Introduction/Karatsuba/Haskell/spec/KaratsubaSpec.hs
+++ Ch1-Introduction/Karatsuba/Haskell/spec/KaratsubaSpec.hs
@@ -5,15 +5,16 @@ import Test.QuickCheck
 import Karatsuba as K
 import Karatsuba0 as K0
 
--- -- configure quickcheck
--- customCheck p = quickCheckWith (stdArgs{ maxSuccess = whatever  }) p
-
 main :: IO ()
 main = hspec $ parallel $ do
   describe "K0.karatsuba" $ do
     it "returns the product of the arguments" $ property $
       \x y -> K0.karatsuba x y == x*y
+    it "returns the product of very large argements" $ do
+      K0.karatsuba 21340987563249087947532356 90985623498562498576492386 `shouldBe` 21340987563249087947532356*90985623498562498576492386
   describe "K.Karatsuba" $ do
     it "returns the product of the arguments" $ property $
       \x y -> K.karatsuba x y == x*y
+    it "returns the product of very large argements" $ do
+      K.karatsuba 21340987563249087947532356 90985623498562498576492386 `shouldBe` 21340987563249087947532356*90985623498562498576492386
 
