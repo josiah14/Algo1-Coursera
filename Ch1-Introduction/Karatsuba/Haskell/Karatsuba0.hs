64f0bd3039bee0a79d48a50cfd9f757e71614d4e
josiah14 <praenato14@gmail.com>
2015-03-13 22:32:14 -0500

    add fully functional example to contrast good vs bad practice in big integer values

---
+++ Ch1-Introduction/Karatsuba/Haskell/Karatsuba0.hs
@@ -0,0 +1,27 @@
+module Karatsuba0 where
+import Data.Bits (bitSize)
+
+karatsuba :: Integer -> Integer -> Integer
+karatsuba x y =  z2 * (10 ^ (2 * m)) + (z1 - z2 - z0) * (10 ^ m) + z0
+  where m = n `div` 2
+        n = max (numDigits x) $ numDigits y
+        (z2, z1, z0) = if maxDigits x y > (bitSize (1 :: Int)) 
+                       then ( karatsuba x1 y1
+                            , karatsuba (x0 + x1) (y0 + y1)
+                            , karatsuba x0 y0
+                            )
+                       else ( x1 * y1
+                            , (x0 + x1) * (y0 + y1)
+                            , x0 * y0
+                            )
+                       where ((x1, x0), (y1, y0)) = (split m x, split m y)
+
+numDigits :: Integer -> Int
+numDigits x = head $ dropWhile (\i -> x `div` 10^i > 0) [0..]
+
+maxDigits :: Integer -> Integer -> Int
+maxDigits x y = max (numDigits x) $ numDigits y
+
+split :: Int -> Integer -> (Integer, Integer)
+split i num = quotRem num (10^i)
+
--- Ch1-Introduction/Karatsuba/Haskell/spec/KaratsubaSpec.hs
+++ Ch1-Introduction/Karatsuba/Haskell/spec/KaratsubaSpec.hs
@@ -2,11 +2,18 @@ module KaratsubaSpec where
 
 import Test.Hspec
 import Test.QuickCheck
-import Karatsuba
+import Karatsuba as K
+import Karatsuba0 as K0
 
 -- -- configure quickcheck
 -- customCheck p = quickCheckWith (stdArgs{ maxSuccess = whatever  }) p
 
 main :: IO ()
 main = hspec $ parallel $ do
-  describe "karatsuba" $ do
+  describe "K0.karatsuba" $ do
+    it "returns the product of the arguments" $ property $
+      \x y -> K0.karatsuba x y == x*y
+  describe "K.Karatsuba" $ do
+    it "returns the product of the arguments" $ property $
+      \x y -> K.karatsuba x y == x*y
+
