4e097e18aba6f4a434a5b803074463d8f4bb260f
josiah14 <praenato14@gmail.com>
2015-03-16 22:11:42 -0500

    python karatsuba: build tree up to bottom-most full level

--- Ch1-Introduction/Karatsuba/Python2/karatsuba.py
+++ Ch1-Introduction/Karatsuba/Python2/karatsuba.py
@@ -1,4 +1,5 @@
 import itertools
+import math
 
 def karatsuba(x, y):
     # Python doesn't implement TCO, so use a loop instead
@@ -7,35 +8,59 @@ def karatsuba(x, y):
 def num_digits(n):
     return itertools.dropwhile(lambda exp: n / 10**exp > 0, itertools.count()).next()
 
-def split(n, m):
-    pow_ten = 10**m
-    high_digits = n / pow_ten
-    low_digits = n - high_digits*pow_ten
-    return high_digits, low_digits
+def split(number, index):
+    pow_ten = 10**index
+    high_digits = number / pow_ten
+    low_digits = number - high_digits*pow_ten
+    return [high_digits, low_digits]
 
 # This calculation is Monoidic and can easily be threaded with a little thought
-class SplitTree(n, m):
-    def __init__(self, number, digit_count):
-        self._number = number
-        self._digit_count = digit_count
-        self._base_leaf_locations = []
+class BinaryRecursionTree:
+    def __init__(self, datum, counter, mapper, reducer):
+        self._datum = datum
+        self._datum_size = counter(datum)
+        self._base_nodes = []
+        self._base_nodes_count = None
+        self._last_full_level_size = None
+        self._height = None
+        self._m = self._datum_size / 2
+        self._tree = mapper(self._datum, self._m)
+        self._mapper = mapper
+        self._reducer = reducer
+        self._counter = counter
+
+    class ref:
+        def __init__(self, obj): self.obj = obj
+        def get(self):           return self.obj
+
+    def _build_tree(self):
+        nodes = [self.ref(self._tree)]
+        nodes0 = []
+        for i in range(0, self.height() - 1):
+            for j in range(0, 2**i):
+                for leaf in range(0,2):
+                    num = nodes[j].get()[leaf]
+                    nodes[j].get()[leaf] = self._mapper(num, self._counter(num) / 2)
+                    nodes0.append(self.ref(nodes[j].get()[leaf]))
+            nodes = nodes0
+            nodes0 = []
 
     def build(self):
         for index in range(0, self.base_nodes_count()):
-            self._base_leaf_locations.append([int(char) for char in bin(index)[:1:-1]])
-            while len(self._base_leaf_locations) < self.height(): self._base_leaf_locations[index].append(0)
+            self._base_nodes.append([int(char) for char in bin(index)[:1:-1]])
+            while len(self._base_nodes[index]) < self.height(): self._base_nodes[index].append(0)
         # the rest of the leaves can be found by continuing to iterate through the range(index, self.base_nodes_count())
         # with self.height() - 1 list sizes
 
     def base_nodes_count(self):
-        if self._base_nodes_count == None: self._base_nodes_count = self._digit_count - self.last_full_level_size()
+        if self._base_nodes_count == None: self._base_nodes_count = self._datum_size - self.last_full_level_size()
         return self._base_nodes_count
 
     def last_full_level_size(self):
-        self._last_full_level_size = self._last_full_level_size or 2**(self.height())
+        self._last_full_level_size = self._last_full_level_size or int(2**(self.height()))
         return self._last_full_level_size
 
     def height(self):
-        self._height = self._height or math.ceil(math.log(self._digit_count, 2)) - 1
+        self._height = self._height or int(math.ceil(math.log(self._datum_size, 2)) - 1)
         return self._height
 
