2407f85599f7055479abfe970626042072d8e2e2
Josiah Berkebile <jgberkebile@dstsystems.com>
2016-01-12 16:10:53 -0600

    full algorithm in place - ScalaCheck found a stack overflow error with args -9223372036854775808 and 0

--- Ch1-Introduction/Karatsuba/Scala/Karatsuba/src/main/scala/ind/josiah/karatsuba/Karatsuba.scala
+++ Ch1-Introduction/Karatsuba/Scala/Karatsuba/src/main/scala/ind/josiah/karatsuba/Karatsuba.scala
@@ -1,11 +1,31 @@
 package ind.josiah.karatsuba
 
+import math.{max, sqrt}
+
 object Karatsuba {
-  def karatsuba(x: (BigInt, String), y: (BigInt, String)): BigInt = {
-    x._1 * y._1
+  def karatsuba(x: BigInt, y: BigInt): BigInt = {
+    val n = max(numDigits(x), numDigits(y))
+    val m = n / 2
+    val (z2, z1, z0) = {
+      val ((x1, x0), (y1, y0)) = (split(m, x), split(m, y))
+      if (x.abs.max(y.abs)
+            > sqrt(Math.pow(2, System.getProperty("sun.arch.data.model").toLong - 1) - 1).floor.round) {
+        ( karatsuba(x1, y1)
+        , karatsuba(x0 + x1, y0 + y1)
+        , karatsuba(x0, y0)
+        )
+      } else {
+        ( x1 * y1
+        , (x0 + x1) * (y0 + y1)
+        , x0 * y0
+        )
+      }
+    }
+
+    z2 * BigInt(10).pow(2 * m) + (z1 - z2 - z0) * BigInt(10).pow(m) + z0
   }
 
-  private[this] def numDigits(x: String): Int = Stream.from(0).dropWhile(i => (x / Math.pow(10, i)).toInt > 0)
+  private[this] def numDigits(num: BigInt): Int = Stream.from(0).dropWhile(i => num / BigInt(10).pow(i) > 0).head
 
-  private[this] def split(i: Int, num: BigInt): (BigInt, BigInt) = num /% Math.pow(10, i).toInt
+  private[this] def split(i: Int, num: BigInt): (BigInt, BigInt) = num /% BigInt(10).pow(i)
 }
--- Ch1-Introduction/Karatsuba/Scala/Karatsuba/src/test/scala/KaratsubaSpec.scala
+++ Ch1-Introduction/Karatsuba/Scala/Karatsuba/src/test/scala/KaratsubaSpec.scala
@@ -5,19 +5,13 @@ import ind.josiah.karatsuba._
 
 class KaratsubaSpec extends FlatSpec with Matchers {
   "karatsuba" should "return the correct product for generic numbers" in {
-    check((x: BigInt, y: BigInt) => Karatsuba.karatsuba((x, x.toString), (y, y.toString)) == x * y)
+    check((x: BigInt, y: BigInt) => Karatsuba.karatsuba(x, y) == x * y)
   }
 
   "karatsuba" should "return the correct product for very large numbers" in {
     assert(
-      Karatsuba.karatsuba(
-        (BigInt.apply("21340987563249087947532356"), "21340987563249087947532356"),
-        (BigInt.apply("90985623498562498576492386"), "90985623498562498576492386")
-      ) == (
-        BigInt.apply("21340987563249087947532356")
-        * BigInt.apply("90985623498562498576492386")
-      )
-    )
+      Karatsuba.karatsuba(BigInt.apply("21340987563249087947532356"), BigInt.apply("90985623498562498576492386"))
+        == BigInt.apply("21340987563249087947532356") * BigInt.apply("90985623498562498576492386"))
   }
 
 }
